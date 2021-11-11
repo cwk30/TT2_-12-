004c243edfe4f2f5dd4161ea11794bbfea48b722
Wen Han <shanegohwenhan@gmail.com>
2021-11-11 11:24:13 +0800

    feature/login

---
+++ frontend/src/PublicRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isLogin } from "./utils";
+
+const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        true && restricted ? <Redirect to="/Login" /> : <Component {...props} />
+      }
+    />
+  );
+};
+
+export default PublicRoute;
